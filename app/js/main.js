a06bb9ca64f7c1c727f36cd408d42f99fa229814
YuryGrivickiy <griv1993@gmail.com>
2020-08-18 11:41:39 +0300

    home page 8/18/2020

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,15 @@
-$(function(){
+let hamburger = document.querySelector('.header-hamburger');
+let curtain = document.querySelector('.curtain');
+let close = document.querySelector('.close');
+let menu = document.querySelector('.header-navbar');
 
-
-
-  
-});
+hamburger.addEventListener('click',()=>{
+	hamburger.classList.toggle('active');
+	curtain.classList.toggle('active');
+	menu.classList.toggle('active-menu');
+})
+close.addEventListener('click',()=>{
+	hamburger.classList.remove('active');
+	curtain.classList.remove('active');
+  	menu.classList.remove('active-menu');
+})
